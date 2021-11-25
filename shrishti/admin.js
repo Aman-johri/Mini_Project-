50c37106991de8414daaa128939f43c6c7ef6871
ShrihtiSingh15 <shrishti.singh_cs19@gla.ac.in>
2021-11-25 18:06:33 +0530

    commit by shrishti

--- shrishti/admin.js
+++ shrishti/admin.js
@@ -18,3 +18,15 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+function login()
+{
+    var email = document.getElementById("raktsangrahalay@gmail.com").value;
+    var password = document.getElementById("123456").value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+        window.location.href = "instruction.html";
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+    });
+}
